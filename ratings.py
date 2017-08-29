d72c824315bfc08b0e2ce3f7418770cea784c98e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-28 18:00:53 -0700

    3 functions working.

--- ratings.py
+++ ratings.py
@@ -1,8 +1,7 @@
 """Restaurant rating lister."""
 
-
-def print_ratings(rate_file):
-    """Takes in a file, prints out restaurants and ratings.
+def make_rating_dict(rate_file):
+    """Takes in a file, returns a dict.
     """
 
     rests = open(rate_file)
@@ -12,10 +11,42 @@ def print_ratings(rate_file):
         ratings = pair.rstrip().split(':')
         new_dict[ratings[0]] = ratings[1]
 
-    restaurants = sorted(new_dict)
+    return new_dict
 
-    for place in restaurants:
+
+new_dict = make_rating_dict('scores.txt')
+
+
+
+
+
+def print_ratings(new_dict):
+    """Takes in a file, prints out restaurants and ratings.
+    """
+
+    restaurant_list = sorted(new_dict)
+
+    for place in restaurant_list:
         print place, 'is rated at', new_dict[place]
 
 
-print_ratings('scores.txt')
+def new_ratings(new_dict):
+    """Allows new input for restaurants/ratings to dict from print_ratings.
+    """
+
+    restaurants = new_dict
+
+    new_rest = raw_input('What restaurant do you want to add?: ')
+    new_rate = raw_input('What is your rating for {}?: '.format(new_rest))
+
+    restaurants[new_rest] = new_rate
+
+    new_dict = restaurants
+
+    return new_dict
+
+
+
+print_ratings(new_dict)
+new_ratings(new_dict)
+print_ratings(new_dict)
